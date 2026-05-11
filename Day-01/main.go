b44e12363fa2af9a7e8629e5128fd124b035912d
Dnyanesh0902 <dnyaneshwarkokatevip@gmail.com>
2026-05-11 21:45:25 +0530

    first commit

---
+++ Day-01/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Go Lang!")
+	fmt.Println("My Name is Dnyaneshwar Kokate")
+	fmt.Println("I am starting Go 90 days challenge")
+
+	var name string = "Dnyaneshwar"
+	var age int = 25
+	var isDeveloper bool = true
+	var salary float64 = 50000.50
+	
+	fmt.Println("Name:", name)
+	fmt.Println("Age:", age)
+	fmt.Println("Is Developer:", isDeveloper)
+	fmt.Println("Salary:", salary)
+	city := "Pune"
+	fmt.Println("City:", city)
+}
